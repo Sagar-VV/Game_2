fe3915d6b503cb9ac6ef99d4dacdd58fe6b5f6bd
Sagar-VV <78678275+Sagar-VV@users.noreply.github.com>
2021-03-14 11:53:33 +0530

    Update Damshraas_game.py
    improved syntax.

--- Damshraas_game.py
+++ Damshraas_game.py
@@ -1,44 +1,44 @@
 # dictonaries = color, car , movie, bike , country
 
 color = {
-    'Red' : 'Symbol of blood'
-    'Blue': 'Color of sky'
-    'Black': 'That everyone likes to wear'
-    'Orange': 'Good for vitamin C'
-    'Yellow': 'Color of Brazil football team jearsy'
+            'Red'   : 'Symbol of blood',
+            'Blue'  : 'Color of sky',
+            'Black' : 'That everyone likes to wear',
+            'Orange': 'Good for vitamin C',
+            'Yellow': 'Color of Brazil football team jearsy'
 }
 
 Car = {
-    'Buggati' : 'Fastest car in the world'
-    'Ferrari': 'Have horse in their company logo'
-    'Rollsroyce': 'Known for its most luxirous car '
-    'Tesla': 'Electric cars'
+            'Buggati' : 'Fastest car in the world',
+            'Ferrari': 'Have horse in their company logo',
+            'Rollsroyce': 'Known for its most luxirous car ',
+            'Tesla': 'Electric cars'
 
 }
 
 Movie = {
-    'Stree' : 'A haunted movie'
-    'I am legend' : 'Zombie apoclaypes'
-    'I am kalam' : ' Movie made on abdul kalam'
-    'Bahubali': 'Prabhas hit movie'
+            'Stree' : 'A haunted movie',
+            'I am legend' : 'Zombie apoclaypes',
+            'I am kalam' : ' Movie made on abdul kalam',
+            'Bahubali': 'Prabhas hit movie'
         
 }
 
 Bike = {
-    'Hayabhusa': 'Faster than most of the bike'
-    'Platina': 'Bike that has maximum average'
-    'Intruder': 'Suzuki flagship bike'
-    'Himalayan' : 'Off roading bike from royal enfield'
+            'Hayabhusa': 'Faster than most of the bike',
+            'Platina': 'Bike that has maximum average',
+            'Intruder': 'Suzuki flagship bike',
+            'Himalayan' : 'Off roading bike from royal enfield'
 
 }
 
 Country = {
 
-    'India': 'Known for its value and tradition'
-    'Russia': 'Known for its cold weather'
-    'Europe': 'Known for its standard of living'
-    'Canada': 'Second country for indians'
-    'Pakistan': 'Similar to india but neighbouring country'
+            'India': 'Known for its value and tradition',
+            'Russia': 'Known for its cold weather',
+            'Europe': 'Known for its standard of living',
+            'Canada': 'Second country for indians',
+            'Pakistan': 'Similar to india but neighbouring country'
 
 }
 
